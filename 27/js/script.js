ebe3f0f046e81e680f9aff1f62d2010878017762
Dmytro Notevskyi <dnotevsky@gmail.com>
2024-08-10 15:14:38 +0300

    lesson 27

---
+++ 27/js/script.js
@@ -0,0 +1,14 @@
+const logg = 'Hello World';
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
