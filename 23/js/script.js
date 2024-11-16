bc5d877c0e750f6bbb61f59f6e015d93821a160a
Dmytro Notevskyi <dnotevsky@gmail.com>
2024-11-16 22:44:51 +0200

    added trim method to the practice from lesson 23

--- 23/js/script.js
+++ 23/js/script.js
@@ -22,7 +22,7 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        const lastFilm = prompt('Один из последних просмотренных фильмов?', '').trim(),
             filmRate = prompt('На сколько оцените его?', '');
         
         if (lastFilm != '' && lastFilm != null && filmRate != '' && filmRate != null && lastFilm.length <= 50) {
