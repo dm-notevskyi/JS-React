e0d04a276573e0bc1ad092f5796e774d496eee78
Dmytro Notevskyi <dnotevsky@gmail.com>
2024-11-16 22:39:08 +0200

    tasks from lesson 28

--- 28/js/script.js
+++ 28/js/script.js
@@ -11,8 +11,7 @@ function calculateVolumeAndArea(val) {
 
 console.log(calculateVolumeAndArea(NaN));
 
-
-
+//=========================================================
 
 function getCoupeNumber(num) {
     if (typeof(num) !== 'number' || !Number.isInteger(num) || num < 0) {
@@ -25,3 +24,85 @@ function getCoupeNumber(num) {
 }
 
 console.log(getCoupeNumber(-10));
+
+//=========================================================
+
+function getTimeFromMinutes(num) {
+    if (typeof(num) !== 'number' || !Number.isInteger(num) || num < 0) {
+        return 'Ошибка, проверьте данные';
+    }
+
+    let hours = Math.floor(num / 60);
+    let minutes = num % 60;
+
+    let hoursWord = '';
+
+    switch (hours) {
+        case 0:
+            hoursWord = 'часов';
+            break;
+        case 1:
+            hoursWord = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursWord = 'часа';
+            break;
+        default:
+            hoursWord = 'часов';
+            break;
+    }
+
+    return `Это ${hours} ${hoursWord} и ${minutes} минут`;
+}
+
+console.log(getTimeFromMinutes(310));
+
+//=========================================================
+
+function findMaxNumber(a, b, c, d) {
+    if (arguments.length < 4 || typeof(a) !== 'number' || typeof(b) !== 'number'  || typeof(c) !== 'number'  || typeof(d) !== 'number') {
+        return 0;
+    }
+
+    let maxNum = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > maxNum) {
+            maxNum = arguments[i];
+        }
+    }
+
+    return maxNum;
+}
+
+console.log(findMaxNumber(1, 5, '6', '10'));
+
+//=========================================================
+
+function fib(num) {
+    if (typeof(num) !== 'number' || !Number.isInteger(num) || num < 1) {
+        return '';
+    }
+
+    let result = '';
+    let first = 0;
+    let second = 1;
+
+    for (let i = 0; i < num; i++) {
+        if (i + 1 === num) {
+            result += `${first}`;
+        } else {
+            result += `${first} `;
+        }
+
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return result;
+}
+
+console.log(fib(4));
