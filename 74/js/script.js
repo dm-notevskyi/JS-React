70e6d02f32a191c8610f4a3545a5ab592341a66a
Dmytro Notevskyi <dnotevsky@gmail.com>
2024-11-29 13:51:30 +0200

    Lesson 74. Functions - constructors

---
+++ 74/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Функція - конструктор
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function() {
+    console.log(`Юзер ${this.name} пішов`);
+}
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
