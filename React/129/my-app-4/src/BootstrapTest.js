d05142060450c769bce94ebdfd0102e16f15ae27
Dmytro Notevskyi <dnotevsky@gmail.com>
2025-01-04 15:44:06 +0200

    Lessong 143. Bootstrap for React

---
+++ React/129/my-app-4/src/BootstrapTest.js
@@ -0,0 +1,104 @@
+import { Container, Row, Col, Carousel, Form, Button } from 'react-bootstrap';
+
+const BootstrapTest = () => {
+    return (
+        <Container className="mt-5 mb-5">
+            <Row>
+                <Col>
+                    <Form>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                        </Row>
+
+                        <Form.Group className="mb-3" controlId="formGridAddress1">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control placeholder="1234 Main St" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formGridAddress2">
+                            <Form.Label>Address 2</Form.Label>
+                            <Form.Control placeholder="Apartment, studio, or floor" />
+                        </Form.Group>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridCity">
+                                <Form.Label>City</Form.Label>
+                                <Form.Control />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridState">
+                                <Form.Label>State</Form.Label>
+                                <Form.Select defaultValue="Choose...">
+                                    <option>Choose...</option>
+                                    <option>...</option>
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridZip">
+                                <Form.Label>Zip</Form.Label>
+                                <Form.Control />
+                            </Form.Group>
+                        </Row>
+
+                        <Form.Group className="mb-3" id="formGridCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                <Col>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://www.aaronreedphotography.com/images/xl/Sweet-Dreams-2022.jpg"
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=2210&quality=70"
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="https://nt.global.ssl.fastly.net/binaries/content/gallery/website/national/library/our-cause/on-the-shore-borrowdale-and-derwent-water-1518851.jpg"
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default BootstrapTest;
--- React/129/my-app-4/src/index.js
+++ React/129/my-app-4/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { styled } from 'styled-components';
 import { Button } from './App';
+import BootstrapTest from './BootstrapTest';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Ми можемо наслідувати імпортований компонент, щоб додати йому індивідуальні стилі:
 const BigButton = styled(Button)`
@@ -17,5 +20,6 @@ root.render(
     <App />
     {/* Можна поміняти тег компоненту за допомогою атрибуту as="a" */}
     <BigButton as="a">Send Report</BigButton>
+    <BootstrapTest></BootstrapTest>
   </React.StrictMode>
 );
